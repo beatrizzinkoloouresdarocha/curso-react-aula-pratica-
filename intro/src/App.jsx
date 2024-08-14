9986512e26044b8595ea82d332cba150464d1639
Beatriz Zinko <zinkobeatriz@gmail.com>
2024-08-14 13:38:28 -0300

    concluido a primeira aula e atividade pratica.

---
+++ intro/src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  // eslint-disable-next-line no-empty-pattern
+  const [] = useState(0)
+
+  return (
+    <div className="App">
+        <h1>Hello word</h1>
+    </div>   
+  )
+}
+
+export default App
