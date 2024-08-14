59ed40f8d28eb17f2081e72727ade5b8fc8b10af
Beatriz Zinko <zinkobeatriz@gmail.com>
2024-08-14 16:46:05 -0300

    concluido os exercicio testei funcionou agora vou começar outra aula .

--- intro/2-Fundamentos_Base/Fundamentos_Base/src/Components/TemplateExpression.jsx
+++ intro/2-Fundamentos_Base/Fundamentos_Base/src/Components/TemplateExpression.jsx
@@ -1,9 +1,22 @@
 // 4-template Expression//
 
 const TemplateExpression =() => {
-    return(
-        <p>Soma é {2+2}</p>
-    )
+const name= "Beatriz";
+
+const data={
+    age:23,
+    job:"Programador",
 }
+
+    return(
+        <div>
+        <p>Soma é {2+2}</p>
+        <h3>Seja bem vindo{name}</h3>
+        <p>
+        sua idade {data.age}anos e voçê é um {data.job}
+        </p>
+        </div>
+    );
+};
 
 export default TemplateExpression
