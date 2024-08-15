31d1e2736aeb22ce84cc51c589d95c7ed1b52051
Beatriz Zinko <zinkobeatriz@gmail.com>
2024-08-15 17:41:22 -0300

    finalizados o quarto exercicio com todas informações testei e funcionou .

---
+++ intro/2-Fundamentos_Base/Fundamentos_Base/3-Avançando-em -React/Avançando-em-React/4-CSS_REACT/CSS_REACT/src/Components/MyComponent.jsx
@@ -0,0 +1,13 @@
+// eslint-disable-next-line no-unused-vars
+import React from "react";
+
+const MyComponent =()=>{
+    return(
+        <div>
+            <h1>CSS de componente</h1>
+            <p>Este paragrafo vai ter um estilo</p>
+        </div>
+    )
+}
+
+export default  MyComponent
